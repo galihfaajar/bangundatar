16bf2372cc450c09a7947c2433d598bb89c0e9c3
galihfaajar <galihfjrr@gmail.com>
2023-11-16 14:27:02 +0700

    update code

---
+++ android/app/src/main/kotlin/com/galih/bangun_datar_kelas_b/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.galih.bangun_datar_kelas_b
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/page/home_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page",
+        style: TextStyle(
+          color: Colors.black
+          ),
+        ),
+        backgroundColor: Colors.amber,
+      ),
+      body: ListView(
+        children: [
+          Row(
+            children: [
+              Expanded(child: CustomMenu(imageAsset:"assets/persegi.jpg", title: "Persegi")),
+              Expanded(child: CustomMenu(imageAsset:"assets/persegipanjang.jpg", title: "Persegi Panjang")),
+            ],
+          ), Row(
+            children: [
+              Expanded(child: CustomMenu(imageAsset:"assets/persegi.jpg", title: "Persegi")),
+              Expanded(child: CustomMenu(imageAsset:"assets/persegipanjang.jpg", title: "Persegi Panjang")),
+            ],
+          ), Row(
+            children: [
+              Expanded(child: CustomMenu(imageAsset:"assets/persegi.jpg", title: "Persegi")),
+              Expanded(child: CustomMenu(imageAsset:"assets/persegipanjang.jpg", title: "Persegi Panjang")),
+            ],
+          ),
+          Row(
+            children: [
+              CustomMenu(imageAsset:"assets/lingkaran.jpg", title: "Lingkaran"),
+
+            ],
+          ),
+          Row(
+            children: [
+              CustomMenu(imageAsset:"assets/lingkaran.jpg", title: "Lingkaran"),
+
+            ],
+          ),
+          Row(
+            children: [
+              CustomMenu(imageAsset:"assets/segitiga.jpg", title: "Segitiga"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomMenu extends StatelessWidget {
+  const CustomMenu({
+    super.key, required this.imageAsset, required this.title,
+  });
+
+  final String imageAsset;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.yellow,
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+        child: Column(
+          children: [
+            Image.asset(imageAsset, height: 484),
+            Text(title, style: TextStyle(color: Colors.black),
+            ),
+          ],
+        ),
+    );
+  }
+}
